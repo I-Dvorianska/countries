927bd60b4da05003542abc17180b6e31a61d5d8b
Alex <alexmatvichuk@gmail.com>
2021-07-16 20:13:09 +0200

    markup for one country and for list

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,5 @@
+import countryCard from '../templates/countryCard.hbs';
+import countries from '../templates/countriesList.hbs';
 export default function fetchCountries(searchQuery) {
   return fetch(searchQuery)
     .then(response => {
@@ -6,6 +8,19 @@ export default function fetchCountries(searchQuery) {
       }
       return response.json();
     })
-    .then(data => console.log(data))
+    .then(country => {
+      console.log(country);
+      if (country.length === 1) {
+        const markup = countryCard(country);
+        console.log(markup);
+      }
+      if (country.length >= 2 && country.length <= 10) {
+        const listMarkup = countries(country);
+        console.log(listMarkup);
+      }
+      if (country.length > 10) {
+        alert('Too many matches found. Please enter a more specific name.');
+      }
+    })
     .catch(error => console.log('it`s 404'));
 }
--- src/js/makeCards.js
+++ src/js/makeCards.js
@@ -1,15 +1,22 @@
 import fetchCountries from './fetchCountries';
-import countryCard from '../templates/countryCard.hbs';
+import countries from '../templates/countriesList.hbs';
+import debounce from 'lodash/debounce';
+
+const DEBOUNCE_DELAY = 300;
 
 const input = document.getElementById('search-box');
+const coutriesList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-input.addEventListener('input', _.debounce(onSearch, 300));
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(e) {
   e.preventDefault();
 
   const inputValue = e.target.value;
-  fetchCountries(
-    `https://restcountries.eu/rest/v2/name/${inputValue}?fields=name;capital;population;flag;languages`,
-  );
+  if (inputValue.length > 0) {
+    fetchCountries(
+      `https://restcountries.eu/rest/v2/name/${inputValue}?fields=name;capital;population;flag;languages`,
+    );
+  }
 }
